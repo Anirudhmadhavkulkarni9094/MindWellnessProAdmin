ae86448ff8081f497cb7606dbc5964e0cff472b1
Anirudhmadhavkulkarni9094 <Anirudhkulkarni9094@gmail.com>
2024-01-01 22:15:41 +0530

    added

--- src/Component/GoBack.js
+++ src/Component/GoBack.js
@@ -6,7 +6,7 @@ function GoBack() {
     <div className='p-5 flex justify-between'>
       
         <Link to='/admin' className='flex items-center gap-4 font-bold'><img src={require("../Assets/go-back.png")} className='w-6' alt='goback'></img>Go back</Link>
-        <button className='px-3 py-2 bg-red-800 rounded-lg text-white border-2' onClick={()=>{sessionStorage.clear(); window.location.href = "/"}}>Sign out</button>
+        <button className='px-3 py-2 bg-red-700 hover:bg-red-500  rounded-lg text-white ' onClick={()=>{sessionStorage.clear(); window.location.href = "/"}}>Sign out</button>
     </div>
   )
 }
